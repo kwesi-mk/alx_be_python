b846346383330e66efb741e7ce48b87d35354a6f
Malachy K. Madu <kidakwesta9@gmail.com>
2024-12-20 23:59:50 +0100

    first commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+operand_sum = 10 + 5
+operand_subtract = 10 - 5
+operand_product = 10 * 5
+
+print(f"Sum of {number1} and {number2} is {operand_sum}")
+print(f"Subtraction of {number1} and {number2} is {operand_subtract}")
+print(f"Product of {number1} and {number2} is {operand_product}")
