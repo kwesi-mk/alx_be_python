b4bb29c0c942859a7273c6b225a693dc805fa1a1
Malachy K. Madu <kidakwesta9@gmail.com>
2025-01-12 23:15:02 +0100

    new changes

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -18,6 +18,6 @@ def calculate_future_date():
    current_date = datetime.now().date()
 
    future_date = current_date + timedelta(days=number_of_days)
-   print("Future date: ", future_date)
+   print("Future date: ", future_date.strftime)
 
 calculate_future_date()
