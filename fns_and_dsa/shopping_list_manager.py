b39c6e6707712ab25565e9d163b49f8e889c240e
Malachy K. Madu <kidakwesta9@gmail.com>
2025-01-12 22:45:12 +0100

    new update

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,7 +13,7 @@ def main():
 
         if choice == 1:
             # Prompt for and add an item
-            new_item = input("Enter a new item: ")
+            new_item = input("Enter the item to add: ")
             shopping_list.append(new_item)
         elif choice == 2:
             del_item = input("Enter the item to be removed: ")
