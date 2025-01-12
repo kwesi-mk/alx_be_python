8f18ec4d0582eee3686cd3f7563215422606e7e6
Malachy K. Madu <kidakwesta9@gmail.com>
2025-01-12 21:56:07 +0100

    bug fixes

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,7 +8,7 @@ def perform_operation(num1, num2, operation):
         case "multiply" :
             return num1 * num2
         case "divide":
-            if num2 == 0:
+            if num2 == 0 or num1 == 0:
                 print("Cannot divide by 0")
             else:
                 return num1 / num2
