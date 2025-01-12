643a4074c711d41ae8268428a8a0b3d0878257fe
Malachy K. Madu <kidakwesta9@gmail.com>
2025-01-12 22:14:23 +0100

    bug fixes

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,26 @@
 def perform_operation(num1, num2, operation):
     
-    match operation:
-        case "add" :
-            return num1 + num2
-        case "subtract" :
-            return num1 - num2
-        case "multiply" :
-            return num1 * num2
-        case "divide":
-            if num2 == 0 or num1 == 0:
-                print("Cannot divide by 0")
-            else:
-                return num1 / num2
+    #match operation:
+        #case "add" :
+         #   return num1 + num2
+        #case "subtract" :
+         #   return num1 - num2
+        #case "multiply" :
+         #   return num1 * num2
+        #case "divide":
+         #   if num2 == 0 :
+          #      print("Cannot divide by 0")
+           # else:
+            #    return num1 / num2
+            
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0 :
+            print("Cannot divide by 0")
+        else:
+            return num1 / num2
